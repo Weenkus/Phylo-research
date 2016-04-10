0fbf06d1c6cbc4795baa6c5c5fa425ab40a09324
Vinko Kodzoman <vinko.kodzoman@fer.hr>
2016-04-10 11:41:06 +0200

    SHC prototype finished.

---
+++ scripts/SHC/Clusterer.py
@@ -0,0 +1,52 @@
+import SimHash
+from Bio import SeqIO
+from multiprocessing import Pool
+
+MD5_LENGTH_BIT = 128
+MD5_LENGTH_HEX = 32
+HEX_SCALE = 16
+BINARY_SCALE = 2
+UTF_8 = 'utf-8'
+
+
+def main():
+    eColi_strains = parse_fasta_file('completeGnomes.fasta')
+
+    #with Pool() as pool:
+    #    sims = pool.map(SimHash.simhash, eColi_strains.values())
+    for seq_id, seq in eColi_strains.items():
+       eColi_strains[seq_id] = SimHash.simhash(seq)
+
+    #print(sims)
+    #print(sims.sort())
+    print(eColi_strains)
+    print()
+
+    for k,v in eColi_strains.items():
+        print(k, v)
+
+    #print(hem_distance('00101001010111101011000111100000110000010000001100100001000001010101011111111011110000001101001100011001010111100010010010110110',
+    #                   '00101001011111000010000101100010100000011111001100110010000011010000001011111110100010000001110110011000010011101110011110111110'))
+
+
+def hem_distance(str1, str2):
+    assert len(str1) == len(str2)
+    diffs = 0
+    for ch1, ch2 in zip(str1, str2):
+            if ch1 != ch2:
+                    diffs += 1
+
+    return diffs
+
+
+def parse_fasta_file(file_path):
+    fasta_sequences = SeqIO.parse(open(file_path), 'fasta')
+    eColi_strains = {}
+    for fasta in fasta_sequences:
+        name, sequence = fasta.id, str(fasta.seq)
+        eColi_strains[name] = sequence
+
+    return eColi_strains
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/SHC/SimHash.py
@@ -0,0 +1,51 @@
+import hashlib
+
+MD5_LENGTH_BIT = 128
+MD5_LENGTH_HEX = 32
+HEX_SCALE = 16
+BINARY_SCALE = 2
+UTF_8 = 'utf-8'
+
+SLIDING_WINDOW_LENGTH = 4
+
+
+def simhash(text):
+    sliding_window_length = SLIDING_WINDOW_LENGTH
+    sh = [0] * MD5_LENGTH_BIT
+    for i in range(0, len(text) - sliding_window_length):
+        word = text[i:i+sliding_window_length]
+        bit_hash = hex_to_binary(hash_md5(word), MD5_LENGTH_BIT)
+        i = 0
+        for bit in bit_hash:
+            if bit == '1':
+                sh[i] += 1
+            else:
+                sh[i] -= 1
+            i += 1
+
+    bit_sh_list = sh_to_bit(sh)
+    bit_sh = ''.join(str(v) for v in bit_sh_list)  # Turn list into a string
+    return bit_sh
+
+
+def hash_md5(word):
+    return hashlib.md5(word.encode(UTF_8))
+
+
+def hex_to_binary(hex_value, length):
+    hex_value = bin(int('1'+str(hex_value.hexdigest()), HEX_SCALE))[3:]  # Don't remove leading zeroes
+    while len(hex_value) < length:
+        hex_value = '0' + hex
+
+    return hex_value
+
+
+def sh_to_bit(final_sh):
+    i = 0
+    for bit in final_sh:
+        if int(bit) >= 0:
+            final_sh[i] = 1
+        else:
+            final_sh[i] = 0
+        i += 1
+    return final_sh
