1e6990b898bdfa554b2fbf34615a2601510a6678
Vinko Kodzoman <vinko.kodzoman@fer.hr>
2016-04-15 09:44:35 +0200

    Output file argument implemented.

--- programs/SHC/main.cpp
+++ programs/SHC/main.cpp
@@ -3,6 +3,7 @@
 #include <unordered_map>
 #include <omp.h>
 #include <time.h>
+#include <fstream>
 
 #include "Utils/FastaParser.h"
 #include "SimHash.h"
@@ -71,13 +72,14 @@ int main(int argc, char *argv[]) {
         cout << "Number of threads: " << num_threads << endl;
         if(hash != NULL ) cout << "Hash function: " << hash << endl;
         cout << "Sliding window length: " << sliding_window_size << endl << endl;
+        ofstream output_file_handle(output_file_path);
 
         // Parse input file
         double start = clock();
         unordered_map<string, string> sequence_id_map = FastaParser::parse_fasta_file(input_file_path);
-        cout << "Parsing done in " << (clock()-start) / CLOCKS_PER_SEC << " seconds." << endl;
-        // Comput the simhash
+        cout << "\nParsing done in " << (clock()-start) / CLOCKS_PER_SEC << " seconds." << endl;
 
+        // Comput the simhash
         start = omp_get_wtime();
         #pragma omp parallel num_threads(num_threads)
         {
@@ -90,41 +92,20 @@ int main(int argc, char *argv[]) {
 
             for(auto element = sequence_id_map.begin(); element !=sequence_id_map.end(); ++element, cnt++) {
                 if(cnt % number_of_threads != thread_id) continue;
-                cout << SimHash::sim_hash((*element).second, sliding_window_size, hash_algorithm) << endl;
+
+                string sequance_hash = SimHash::sim_hash((*element).second, sliding_window_size, hash_algorithm);
+                cout << sequance_hash << endl;
+
+                if(output_file_path != NULL) {
+                    output_file_handle << (*element).first << endl << sequance_hash << endl << endl;
+                }
             }
         }
         cout << "SimHashes created in " << (omp_get_wtime()-start) << " seconds." << endl;
 
 
-
+        cout << "\nProgram is finished. (press any key to exit)" << endl;
         std::cin.get();
         return 0;
     }
-
-
-
-    double start = clock();
-    string file_path{FastaParser::initialise_fasta_file_path(argc, argv)};
-    unordered_map<string, string> sequence_id_map = FastaParser::parse_fasta_file(file_path);
-    cout << "Parsing done in " << (clock()-start) / CLOCKS_PER_SEC << " seconds." << endl;
-
-    start = omp_get_wtime();
-    #pragma omp parallel
-    {
-        size_t cnt = 0;
-        int thread_id = omp_get_thread_num();
-        int number_of_threads = omp_get_num_threads();
-
-        if(thread_id == 0)
-            cout << "Using " << number_of_threads << " threads to compute " << sequence_id_map.size() << " simhashs." << endl;
-
-        for(auto element = sequence_id_map.begin(); element !=sequence_id_map.end(); ++element, cnt++) {
-            if(cnt % number_of_threads != thread_id) continue;
-            cout << SimHash::sim_hash((*element).second, 8, SimHash::HashAlgorithm::Native) << endl;
-        }
-    }
-
-    cout << "SimHashes created in " << (omp_get_wtime()-start) << " seconds." << endl;
-
-    return 0;
 }
