128b69ec4fc8e96d8f1d14a10eef0479a012520e
Vinko Kodzoman <vinko.kodzoman@fer.hr>
2016-05-13 17:51:17 +0200

    Added corelation graphs for no blosum matrix

--- scripts/Blast_SimHash_Compare/Blast_SQ_generate_compare_file.py
+++ scripts/Blast_SimHash_Compare/Blast_SQ_generate_compare_file.py
@@ -1,17 +1,17 @@
 HASH_LENGTH = 64
 SEQUENCE_ID_LEN = 60
-HASH = 'Spooky'
+HASH = 'Boost'
 WINDOWS_SIZE = 3
 
 
 blast_hits_file_path = "/home/weenkus/workspace/Phylo-research/results_simHash/blast_comparison/" \
-                       "ENSP233/ENSP00000000233_hits_blast.txt"
+                       "ENSP1146/ENSP00000001146_hits_blast.txt"
 
 hash_hits_file_path = "/home/weenkus/workspace/Phylo-research/results_simHash/blast_comparison/" \
-                      "ENSP233/ENSP233_hits_spooky_3.txt"
+                      "ENSP1146/ENSP1146_hits_boost_3.txt"
 
-output_file_path = "ENSP233_Spooky_3_compare.txt"
-csv_comparison = "ENSP233_Spooky_3_compare.csv"
+output_file_path = "ENSP1146_boost_3_compare.txt"
+csv_comparison = "ENSP1146_boost_3_compare.csv"
 
 blast_hits_file = open(blast_hits_file_path, "r")
 hash_hits_file = open(hash_hits_file_path, "r")
@@ -23,7 +23,7 @@ hash_hits_list = hash_hits_file.read().split("\n")
 
 
 porgess = 0
-csv_string = 'window_size,hash,ham_distance,similarity,error,blast_output'
+csv_string = 'window_size,hash,ham_distance,similarity,error,blast_output,bitscore,eValue'
 print(csv_string, file=csv_file)
 
 for blast_hit in blast_hits_list:
@@ -56,14 +56,16 @@ for blast_hit in blast_hits_list:
 
             print(out_string, file=output_file)
 
-            csv_string = '{window_size},{hash},{ham_distance},{similarity},{error:2f},{blast_output}'.format(
+            csv_string = '{window_size},{hash},{ham_distance},{similarity},{error:2f},{blast_output},{bitmap},{eValue}'.format(
                 window_size=WINDOWS_SIZE,
                 hash=HASH,
                 sequence_ids=sequence_ids,
                 ham_distance=hash_string_chunks[2],
                 error=abs(float(sim)-float(blast_string_chunks[2])),
                 similarity=sim,
-                blast_output=blast_string_chunks[2]
+                blast_output=blast_string_chunks[2],
+                bitmap=blast_string_chunks[-1],
+                eValue=blast_string_chunks[-2]
             )
 
             print(csv_string, file=csv_file)
